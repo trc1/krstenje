a10a5520e0f3ab58ed83f7cf3c50b39eba99df47
Dario Trcović <116907511+trc1@users.noreply.github.com>
2023-09-26 21:53:58 +0200

    initial commit

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -1,8 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
-
+const supabaseUrl = "https://sqbdiplkaelmrewzicyx.supabase.co"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNxYmRpcGxrYWVsbXJld3ppY3l4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTU2MzMzMDUsImV4cCI6MjAxMTIwOTMwNX0.YNiFyOY7guuejbI7IkiGWh-1EgHD37qTmKFWGC3zws0"
 const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
 export default supabase
